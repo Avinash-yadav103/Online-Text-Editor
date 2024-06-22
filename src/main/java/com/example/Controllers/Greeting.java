ce3264b60a195f769af86be76fd83ce5dc53d497
Avinash Yadav <130153290+Avinash-yadav103@users.noreply.github.com>
2024-06-22 20:03:12 +0530

    Add files via upload

---
+++ src/main/java/com/example/Controllers/Greeting.java
@@ -0,0 +1,13 @@
+package com.example.Controllers;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Greeting {
+
+    private String message;
+}
---
+++ src/main/java/com/example/Controllers/OpenAI.java
@@ -0,0 +1,18 @@
+// package com.example.Controllers;
+// import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+
+
+// @RestController
+// public class OpenAI {
+
+//     @GetMapping("/getChat/{prompt}")
+//     public String getPrompt(@PathVariable String prompt){
+        
+// 		// v1/completions	text -davinci-003 -model -temperature 0.5 -max_tokens 150 -top_p 1 -frequency_penalty 0.0 -presence_penalty 0.0 -stop
+// 		OpenAiService service = new OpenAiService("");
+//         return null;
+//     }
+    
+// }
